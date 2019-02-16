1eb649640961eab7d60746f7e379ccdb87d55ffa
Agit IŞIK <csd@hotmail.com.tr>
2019-02-16 23:17:29 +0300

    Default Changelist

--- apps/education/models.py
+++ apps/education/models.py
@@ -18,7 +18,7 @@ class Education(AbstractBaseModel):
     link = models.URLField('School URL',blank=True)
     start_date = models.DateField(null=True,blank=True)
     end_date = models.DateField(null=True,blank=True)
-    is_current = models.BooleanField(default=False,blank=True,null=True)
+    #is_current = models.BooleanField(default=False,blank=True,null=True)
     description = models.TextField(blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="education_user")
 
--- reapp/settings.py
+++ reapp/settings.py
@@ -95,7 +95,7 @@ WSGI_APPLICATION = 'reapp.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    'defaultsss': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'dui70o5ng8qqj',
         'USER': 'duwotsdtcyslqc',
@@ -103,7 +103,7 @@ DATABASES = {
         'HOST': 'ec2-54-225-89-195.compute-1.amazonaws.com',
         'PORT': '5432',
     },
-    'defaultsss': {
+    'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
