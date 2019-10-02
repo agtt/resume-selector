3a2b270f1424318cdc97ee127d922ae4fa16fb50
Agit <csd@hotmail.com.tr>
2019-10-03 01:00:17 +0300

    University

--- apps/education/models.py
+++ apps/education/models.py
@@ -20,11 +20,11 @@ class AbstractBaseModel(models.Model):
 
 
 class Education(AbstractBaseModel):
-    name = models.CharField(max_length=250, verbose_name="School Name",null=True, blank=True)
+    name = models.CharField(max_length=250, verbose_name="School Name", null=True, blank=True)
     university = models.ForeignKey(University, on_delete=models.CASCADE, related_name="education_university",
-                                   blank=True,null=True)
-    degree = models.CharField(max_length=250, verbose_name="Degree")
-    chapter = models.CharField(max_length=250, verbose_name="Chapter")
+                                   blank=True, null=True)
+    degree = models.CharField(max_length=250, verbose_name="Degree", null=True, blank=True)
+    chapter = models.CharField(max_length=250, verbose_name="Chapter", null=True, blank=True)
     link = models.URLField('School URL', blank=True)
     start_date = models.DateField(null=True, blank=True)
     end_date = models.DateField(null=True, blank=True)
