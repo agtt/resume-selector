ce41565dc966fc0f0089c43657c98437d62f6da6
Agit <csd@hotmail.com.tr>
2019-10-03 01:14:15 +0300

    University

--- apps/job/models.py
+++ apps/job/models.py
@@ -2,8 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-# Create your models here.
-
 class Industry(models.Model):
     name = models.CharField(max_length=250, verbose_name="Inudstry Name", blank=True)
 
